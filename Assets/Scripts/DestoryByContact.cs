f6ed574cbce0e4f46b96877110e54edae2fbe879
Nicholas Sabelli <nicksabelli1694@gmail.com>
2016-03-20 14:55:57 -0400

    init

---
+++ Assets/Scripts/DestoryByContact.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestoryByContact : MonoBehaviour 
+{
+	public GameObject explosion;
+	public GameObject playerExplotion;
+
+	public int scoreValue;
+
+	private GameController gameController;
+
+	void Start()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+
+		if(gameControllerObject != null)
+		{
+			gameController = gameControllerObject.GetComponent<GameController>();
+		}
+		else if(gameControllerObject == null)
+		{
+			Debug.Log("Cannot find 'GameController' script");
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag != "Boundary")
+		{
+//			Debug.Log(other.name);
+			Instantiate(explosion, transform.position, transform.rotation);
+
+			if(other.tag == "Player")
+			{
+				Instantiate(playerExplotion, other.transform.position, other.transform.rotation);
+				gameController.GameOver();
+			}
+
+			gameController.AddScore(scoreValue);
+			Destroy(other.gameObject);
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public Boundry boundry;
+
+	public float speed;
+	public float tilt;
+	public float fireRate;
+
+	private float nextFire;
+
+	public GameObject shot;
+
+	public Transform shotSpawn;
+
+	private Rigidbody rb;
+
+	private AudioSource audioSource ;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	void Update()
+	{
+		if ((Input.GetButton("Fire1") || Input.GetKeyDown("space")) && Time.time > nextFire) 
+		{
+			nextFire = Time.time + fireRate;
+
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+
+			audioSource.Play();
+		}
+	}
+	 
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal , 0.0f, moveVertical);
+
+		rb.velocity = movement * speed;
+
+		rb.position = new Vector3
+		(
+			Mathf.Clamp(rb.position.x, boundry.xMin, boundry.xMax),
+			0.0f,
+			Mathf.Clamp(rb.position.z, boundry.zMin, boundry.zMax)
+		);
+
+		rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
+}
+
+[System.Serializable]
+public class Boundry
+{
+	public float xMin, xMax, zMin, zMax;
+}
